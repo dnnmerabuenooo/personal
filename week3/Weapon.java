3665b3e076108bf78e0d015665e7747a2b61310b
Dianne Merabueno <111546446+dnnmerabuenooo@users.noreply.github.com>
2022-09-04 21:36:32 +0800

    Update Weapon.java

--- week3/Weapon.java
+++ week3/Weapon.java
@@ -1,24 +1,19 @@
 public class Weapon {
 
-    // Properties/Attributes
-    String name;
+    String type;
     int damage;
     float weight;
     String rarity;
     String description;
     boolean isEternal;
 
-    // Method
     public void showAllAttributes(){
-        System.out.println("Name:" + name);
-        System.out.println("Damage:" + damage);
-        System.out.println("Weight:" + weight);
-        System.out.println("Rarity:" + rarity);
-        System.out.println("Eternal:" + isEternal);
-     }
-
-     public void displayweaponDescription(){
-        System.out.println("Description:" + description);
-     }
+        System.out.println("Type: " + type);
+        System.out.println("Damage: " + damage);
+        System.out.println("Weight: " + weight);
+        System.out.println("Rarity: " + rarity);
+        System.out.println("Eternal: " + isEternal);
+        System.out.println(description);
+    }
     
 }
